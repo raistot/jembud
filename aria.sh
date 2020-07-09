8fe8473d09c12a0cf4b87d5874fef7fcf6b21408
Tomi <tomy.prs25@gmail.com>
2020-07-09 16:49:31 +0700

    ariatools: Use Best Tracker port Switch 8210
    Signed-off-by: Tomi <tomy.prs25@gmail.com>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- aria.sh
+++ aria.sh
@@ -1,7 +1,14 @@
+file="best_aria2.txt"
+if [ -f $file ] ; then
+    rm $file
+fi
+echo "Getting best trackers!"
+wget https://trackerslist.com/best_aria2.txt
+trackerlist=$(cat best_aria2.txt)
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=5
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
-  --max-connection-per-server=10 --rpc-max-request-size=1024M \
+export MAX_CONCURRENT_DOWNLOADS=15
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 8210 \
+  --max-connection-per-server=16 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --daemon=true --allow-overwrite=true --bt-tracker=$trackerlist --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+   --max-overall-upload-limit=0 --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
